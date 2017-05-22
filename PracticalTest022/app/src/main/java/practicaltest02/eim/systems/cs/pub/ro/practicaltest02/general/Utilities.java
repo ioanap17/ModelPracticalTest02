be8d8198afb022977157f061d7349c0fb7a12421
Ioana <ioanap17@gmail.com>
2017-05-22 20:15:22 +0300

    Done

---
+++ PracticalTest022/app/src/main/java/practicaltest02/eim/systems/cs/pub/ro/practicaltest02/general/Utilities.java
@@ -0,0 +1,23 @@
+package practicaltest02.eim.systems.cs.pub.ro.practicaltest02.general;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by Paun on 22.05.2017.
+ */
+
+public class Utilities {
+
+    public static BufferedReader getReader(Socket socket) throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public static PrintWriter getWriter(Socket socket) throws IOException {
+        return new PrintWriter(socket.getOutputStream(), true);
+    }
+
+}
